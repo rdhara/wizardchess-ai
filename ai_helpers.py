9604ce8d1c226376200d217e298afe063c57ca42
Raghu Dhara <rdhara@college.harvard.edu>
2016-11-27 22:20:08 -0500

    basic structure layout

---
+++ ai_helpers.py
@@ -0,0 +1,16 @@
+"""
+Helper functions for voice recognition and board API calls
+"""
+import voicewav
+
+
+def get_next_move():
+    pass
+
+
+def get_voice_recording():
+    pass
+
+
+def interpret_wav():
+    pass
