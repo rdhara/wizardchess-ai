24ed79ad7c02217ed5cd22d24d1d6afdaf00eecd
Raghu Dhara <rdhara@college.harvard.edu>
2016-11-28 19:19:34 -0500

    added function for wav to amplitude

---
+++ word_trainer.py
@@ -0,0 +1,45 @@
+"""
+Find transition probabilities from audio input
+"""
+
+import pyaudio
+import wave
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+
+
+def record_audio(n_sec=5, output=None):
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * n_sec)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    if output:
+        wf = wave.open(output, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+    return frames
